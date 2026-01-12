ae88e33485974703e3b2b017ae9ee3921f6fc714
EnzoMessaoudi <12168@holbertonstudents.com>
2026-01-12 13:10:28 +0100

    ADD factorial.py file

---
+++ debugging/factorial.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
+
+if len(sys.argv) != 2:
+    print("Not enought arguments passed")
+    sys.exit(1)
+
+n = int(sys.argv[1])
+if(n < 0):
+        print("Passed a negative value")
+        sys.exit(1)
+
+f = factorial(n)
+print(f)
